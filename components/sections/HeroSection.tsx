31732db66b656d5eed88fd2b99230afb0e8822b1
George Luo <focusdlaw@gmail.com>
2026-03-12 23:51:14 +0800

    feat: Add shadCN | Add HeroSection

---
+++ components/sections/HeroSection.tsx
@@ -0,0 +1,24 @@
+import { defineQuery } from "next-sanity";
+
+const HERO_QUERY = defineQuery(`*[_id == "singleton-profile"][0]{
+  firstName,
+  lastName,
+  headline,
+  headlineStaticText,
+  headlineAnimatedWords,
+  headlineAnimationDuration,
+  shortBio,
+  email,
+  phone,
+  location,
+  availability,
+  socialLinks,
+  yearsOfExperience,
+  profileImage
+}`);
+
+function HeroSection() {
+  return <div>HeroSection</div>;
+}
+
+export default HeroSection;
